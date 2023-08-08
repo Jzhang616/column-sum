0701a8f734c9703a393e5e7320461c52d1e25b3f
Jason Zhang <141682791+Jzhang616@users.noreply.github.com>
2023-08-08 16:48:16 -0400

    Add files via upload

---
+++ p2.sh
@@ -0,0 +1,18 @@
+#Jason Zhang
+#JASZHANG
+#ID: 112920652
+
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "data file or output file not found"
+else
+	input=$1
+	output=$2
+	> "$output"
+	if [ ! -f "$input" ]; then
+		echo "<$input> not found"
+	else
+		awk -v OFS=" : " -v output_file="$output" 'BEGIN {FS="[;,]"}{for (i=1; i<=NF; i++) {sum[i] += $i}}END {for (i=1; i<=length(sum); i++) {print "Col " i, sum[i] > output_file}}' "$input"
+	fi
+fi
